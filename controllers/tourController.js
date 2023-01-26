4e460be9320a22af82ed7d4269a1689461cc373a
James Niyongira <jamesme85.me@gmail.com>
2023-01-26 16:43:52 -0500

    implemented filtering feature

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,18 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   console.log(req.query);
-  const tours = await Tour.find();
+  // 1. FILTERING
+  const queryObj = { ...req.query };
+
+  const excludedFields = ["page", "sort", "limit", "fields"];
+  excludedFields.forEach((el) => delete queryObj[el]);
+  console.log(queryObj);
+  // 2. ADVANCED FILTERING
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+  const query = Tour.find(JSON.parse(queryStr));
+
+  const tours = await query;
   res.status(200).json({
     status: "success, you are in the tours route",
     data: tours,
