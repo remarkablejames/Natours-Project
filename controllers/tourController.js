336a4d15219627daab87777235d16b8744e8aaf0
James Niyongira <jamesme85.me@gmail.com>
2023-01-26 17:14:59 -0500

    implemented pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,6 +29,17 @@ exports.getAllTours = async (req, res) => {
     query = query.select("-__v");
   }
 
+  // 5. PAGINATION
+  const page = req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 100;
+  const skip = (page - 1) * limit;
+  query = query.skip(skip).limit(limit);
+  if (req.query.page) {
+    const numTours = await Tour.countDocuments();
+    if (skip >= numTours) throw new Error("This page does not exist");
+  }
+
+  // EXECUTE QUERY
   const tours = await query;
   res.status(200).json({
     status: "success, you are in the tours route",
