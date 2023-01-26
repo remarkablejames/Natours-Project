71ee92502e56b77f256ba88a5441bea2566a9787
James Niyongira <jamesme85.me@gmail.com>
2023-01-26 15:11:24 -0500

    added more fields to the tour model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,11 +16,18 @@ exports.getTour = async (req, res) => {
   });
 };
 exports.createTour = async (req, res) => {
-  const newTour = await Tour.create(req.body);
-  res.status(201).json({
-    status: "success, you successfully created a new tour",
-    data: newTour,
-  });
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: "success, you successfully created a new tour",
+      data: newTour,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
 exports.updateTour = async (req, res) => {
