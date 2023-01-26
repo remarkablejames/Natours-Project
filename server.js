d003b43500fef51491c47ffc788f0dbb06699540
James Niyongira <jamesme85.me@gmail.com>
2023-01-26 14:17:06 -0500

    connected application to the mongodb database

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 const port = process.env.PORT || 3000;
-
+//CONNECTING TO THE DATABASE
+const DB = process.env.DATABASE;
+mongoose.connect(DB).then(() => {
+  console.log("DB connection successful");
+});
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
