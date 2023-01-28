205de3863232cabd234277b015482ef5e7d6d3a1
James Niyongira <jamesme85.me@gmail.com>
2023-01-28 05:41:12 -0500

    added more data validation  rules to  tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+// const validator = require("validator");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a name"],
       unique: true,
       trim: true,
+      maxLength: [40, "A tour name must have less or equal then 40 characters"],
+      minLength: [10, "A tour name must have more or equal then 10 characters"],
     },
     slug: String,
     duration: {
@@ -21,11 +24,17 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either: easy, medium, difficult",
+      },
     },
 
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
     },
     ratingsQuantity: {
       type: Number,
@@ -35,7 +44,14 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, "A tour must have a price"],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: function (val) {
+        // this only points to current doc on NEW document creation
+        return val < this.price;
+      },
+      message: "Discount price ({VALUE}) should be below regular price",
+    },
     summary: {
       type: String,
       trim: true,
