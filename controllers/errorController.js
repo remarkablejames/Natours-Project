6e0bdc4f2c86695860a624477b6fcd7065e7eb3b
James Niyongira <42360352+remarkablejames@users.noreply.github.com>
2023-02-06 16:21:07 +0000

    testing git configurations

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -58,4 +58,5 @@ const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
   const message = `Invalid input data. ${errors.join(". ")}`;
   return new AppError(message, 400);
+  //
 };
