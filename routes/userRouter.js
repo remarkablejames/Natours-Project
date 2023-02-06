b6dfdb7f240c4965bf165fde5b95aeed0b1d20ef
James Niyongira <lascasasjames85@gmail.com>
2023-02-06 16:48:07 -0500

    implemented signup feature

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("../controllers/authController");
 
-router.route("/").get((req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "you are in the users route",
-  });
-});
+router.route("/signup").post(authController.signup);
 
 module.exports = router;
