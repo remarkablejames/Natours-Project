d8a9ff62d9cee34753b915d70de76f1ad5426bf0
James Niyongira <lascasasjames85@gmail.com>
2023-02-06 18:48:47 -0500

    implemented password hashing on user signup

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -45,6 +46,17 @@ const userSchema = new mongoose.Schema({
     default: true,
     select: false,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  console.log("here we are");
+  this.password = await bcrypt.hashSync(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
