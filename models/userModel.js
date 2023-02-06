a24855b3df94788f064d404f0c564d693a12dbb5
James Niyongira <lascasasjames85@gmail.com>
2023-02-06 15:06:00 -0500

    added a user model

---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A user must have a name"],
+    trim: true,
+    maxlength: [40, "A user name must have less or equal then 40 characters"],
+    minlength: [10, "A user name must have more or equal then 10 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "A user must have an email"],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  photo: {
+    type: String,
+    default: "default.jpg",
+  },
+  password: {
+    type: String,
+    required: [true, "A user must have a password"],
+    minlength: [8, "A user password must have more or equal then 8 characters"],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "A user must have a password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+    },
+    message: "Passwords are not the same!",
+  },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
