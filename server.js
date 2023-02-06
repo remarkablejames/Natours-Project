56cb8132df7268d909fc1ac7984f82a7e0501c73
James Niyongira <lascasasjames85@gmail.com>
2023-02-06 12:15:32 -0500

    handled all uncaught exceptions

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 dotenv.config({ path: "./config.env" });
-
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNCAUGHT EXCEPTION! Shutting down...");
+  process.exit(1);
+});
 const app = require("./app");
 const port = process.env.PORT || 3000;
 //CONNECTING TO THE DATABASE
-console.log(process.env.NODE_ENV, "-----------------");
 
 const DB = process.env.DATABASE;
 mongoose.connect(DB).then(() => {
@@ -18,6 +21,14 @@ const server = app.listen(port, () => {
 process.on("unhandledRejection", (err) => {
   console.log(err.name, err.message);
   console.log("UNHANDLED REJECTION! Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNCAUGHT EXCEPTION! Shutting down...");
   server.close(() => {
     process.exit(1);
   });
