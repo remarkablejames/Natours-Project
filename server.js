bc00bd215384089c7d30edd1040ba97c2682a062
James Niyongira <lascasasjames85@gmail.com>
2023-02-06 12:00:44 -0500

    handled all unhandled promise rejections

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ const DB = process.env.DATABASE;
 mongoose.connect(DB).then(() => {
   console.log("DB connection successful");
 });
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
 });
