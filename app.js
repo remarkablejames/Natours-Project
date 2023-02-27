1abb50f500eb432833ee49b38ca36aa75e07eaea
James Niyongira <lascasasjames85@gmail.com>
2023-02-27 14:35:59 -0500

    prevented parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
+const hpp = require("hpp");
 const app = express();
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -32,6 +33,19 @@ app.use(express.json({ limit: "10kb" }));
 app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xss());
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
