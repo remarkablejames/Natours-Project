49fd65ec6f4c987a30a19bdb71eca17fd5495ca5
James Niyongira <lascasasjames85@gmail.com>
2023-02-27 14:18:01 -0500

    implemented express-rate-limit feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 const app = express();
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -7,9 +8,17 @@ const globalErrorHandler = require("./controllers/errorController");
 // 3) ROUTES
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
 app.use(express.json());
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
