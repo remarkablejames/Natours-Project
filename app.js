d8dbd1b9a73975fe4831dccd9acf8fedbac5a9e6
James Niyongira <lascasasjames85@gmail.com>
2023-02-27 14:31:14 -0500

    implemented data sanitazation and anti-XSS attacks

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const app = express();
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -9,16 +12,26 @@ const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
 // 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+// Development logging
 app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour!",
 });
 app.use("/api", limiter);
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
