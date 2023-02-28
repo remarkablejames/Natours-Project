c080ed6e5061e383b05a34d2e0f08e72880581d3
James Niyongira <lascasasjames85@gmail.com>
2023-02-28 13:09:27 -0500

    added dev data and geospacial to the tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -71,7 +71,36 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: "Point",
+          enum: ["Point"],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
   },
+
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
