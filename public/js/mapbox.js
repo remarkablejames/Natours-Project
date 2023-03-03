13c03a8f42fd4c145bfe5893b40bc52083c04753
James Niyongira <lascasasjames85@gmail.com>
2023-03-03 16:12:28 -0500

    integrated mapbox into the tours page

---
+++ public/js/mapbox.js
@@ -0,0 +1,48 @@
+const locations = JSON.parse(document.getElementById("map").dataset.locations);
+console.log(locations);
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicmVtYXJrYWJsZWphbWVzIiwiYSI6ImNsZXN6d214ejA1bzgzdG10OTl5cGc3azMifQ.xJUcjDDDzGrbme3z3lD-8w";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/remarkablejames/clet0dsjr001601pggd4ztis9",
+  center: [-75.695, 45.424721],
+  zoom: 12,
+  scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement("div");
+  el.className = "marker";
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: "bottom",
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
